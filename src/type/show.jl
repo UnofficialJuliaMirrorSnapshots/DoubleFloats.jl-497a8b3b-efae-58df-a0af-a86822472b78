acf07bad79ed331b19bcacf98291154b908a52f7
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-23 11:09:21 +0000

    Snapshot of branch master taken on 2019-10-23T07:09:19.989-04:00 from "https://github.com/UnofficialJuliaMirror/DoubleFloats.jl-497a8b3b-efae-58df-a0af-a86822472b78"

--- src/type/show.jl
+++ src/type/show.jl
@@ -48,40 +48,13 @@ showall(x::Complex{DoubleFloat{T}}) where {T<:IEEEFloat} = print(Base.stdout, st
 
 
 if VERSION < v"1.1"
-using Base.Grisu: DIGITSs
-    fix_dec(out, d::Double64, flags::String, width::Int, precision::Int, c::Char) = 
-        fix_dec(out, Float64(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
-    fix_dec(out, d::Double32, flags::String, width::Int, precision::Int, c::Char) = 
-        fix_dec(out, Float32(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
-    fix_dec(out, d::Double16, flags::String, width::Int, precision::Int, c::Char) = 
-        fix_dec(out, Float16(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
-    ini_dec(out, d::Double64, flags::String, width::Int, precision::Int, c::Char) = 
-        ini_dec(out, Float64(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
-    ini_dec(out, d::Double32, flags::String, width::Int, precision::Int, c::Char) = 
-        ini_dec(out, Float32(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
-    ini_dec(out, d::Double16, flags::String, width::Int, precision::Int, c::Char) = 
-        ini_dec(out, Float16(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
-        
     fix_dec(x::Double64, n::Int) = fix_dec(Float64(x), n)
     fix_dec(x::Double32, n::Int) = fix_dec(Float64(x), n)
     fix_dec(x::Double16, n::Int) = fix_dec(Float32(x), n)
     ini_dec(x::Double64, n::Int) = ini_dec(Float64(x), n)
     ini_dec(x::Double32, n::Int) = ini_dec(Float64(x), n)
     ini_dec(x::Double16, n::Int) = ini_dec(Float32(x), n)
-else   
-    fix_dec(out, d::Double64, flags::String, width::Int, precision::Int, c::Char, digits) = 
-        fix_dec(out, Float64(d), flags, width, precision, c, digits)
-    fix_dec(out, d::Double32, flags::String, width::Int, precision::Int, c::Char, digits) = 
-        fix_dec(out, Float32(d), flags, width, precision, c, digits)
-    fix_dec(out, d::Double16, flags::String, width::Int, precision::Int, c::Char, digits) = 
-        fix_dec(out, Float16(d), flags, width, precision, c, digits)
-    ini_dec(out, d::Double64, flags::String, width::Int, precision::Int, c::Char, digits) = 
-        ini_dec(out, Float64(d), flags, width, precision, c, digits)
-    ini_dec(out, d::Double32, flags::String, width::Int, precision::Int, c::Char, digits) = 
-        ini_dec(out, Float32(d), flags, width, precision, c, digits)
-    ini_dec(out, d::Double16, flags::String, width::Int, precision::Int, c::Char, digits) = 
-        ini_dec(out, Float16(d), flags, width, precision, c, digits)
-
+else
     fix_dec(x::Double64, n::Int, digits) = fix_dec(Float64(x), n, digits)
     fix_dec(x::Double32, n::Int, digits) = fix_dec(Float64(x), n, digits)
     fix_dec(x::Double16, n::Int, digits) = fix_dec(Float32(x), n, digits)
